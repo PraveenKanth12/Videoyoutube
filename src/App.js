fa63d6730dcb3d658a013f110b73476c3a2d9ee7
PraveenKanth12 <janjalapraveenkanth12@gmail.com>
2024-09-23 12:42:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import youtube from "./api/youtube.js";
+import { Grid } from "@mui/material";
+import SearchBar from "./components/SearchBar.jsx";
+import { useState } from "react";
+import VideoDetail from "./components/VideoDetail.jsx";
+import VideoList from "./components/VideoList.jsx";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
+
+  async function handleSubmit(searchTerm) {
+    try {
+      const {
+        data: { items: videos },
+      } = await youtube.get("search", {
+        params: {
+          part: "snippet",
+          maxResults: 5,
+          key: "AIzaSyC0NlbgAFLS8mZ_Si50sukT37exIPeiMcc",
+          q: searchTerm,
+        },
+      });
+  
+      setVideos(videos);
+      if (videos.length > 0) {
+        setSelectedVideo(videos[0]); // Set the first video as selected
+      } else {
+        setSelectedVideo(null); // Handle empty video list
+      }
+    } catch (error) {
+      console.error("Error fetching Youtube videos:", error);
+    }
+  }
 
 export default App;
---
+++ src/components/VideoList.jsx
@@ -0,0 +1,15 @@
+import {Grid} from "@mui/material";
+import VideoItem from "./VideoItem";
+
+const VideoList=({videos,onSelectVideo})=>{
+    const listOfVideos=videos.map(video=>(
+        <VideoItem onSelectVideo={onSelectVideo} key={video.id.videoId} video={video}/>
+    ))
+    return(
+        <Grid container spacing={10}>
+            {listOfVideos}
+        </Grid>
+    )
+}
+
+export default VideoList
